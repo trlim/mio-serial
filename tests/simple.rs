f6615167314b1ad124f825f3970839f0a65a4da9
Tae-il Lim <trlim@iotok.kr>
2016-11-06 17:59:19 +0900

    Reimplement test timeout timer using `mio::Timer`

--- tests/simple.rs
+++ tests/simple.rs
@@ -4,24 +4,16 @@ extern crate dotenv;
 
 use self::dotenv::dotenv;
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{Read, Write};
 use std::time::Duration;
 
 use mio::{Events, Poll, Ready, PollOpt, Token};
+use mio::timer::Timer;
 use mio_serial::{SerialPort, PortSettings};
 use mio_serial::{BaudRate, CharSize, Parity, StopBits, FlowControl};
 
-extern crate serial;
-use serial::SerialPort as _SerialPort;
-
-fn setup_serial_port(serial_port: &mut serial::SystemPort) -> io::Result<()> {
-    try!(serial_port.set_timeout(Duration::from_millis(10000)));
-
-    Ok(())
-}
-
 #[test]
-fn serial_port() {
+fn poll_write() {
     dotenv().ok();
 
     let port_name = env::var("SERIAL_PORT")
@@ -38,10 +30,8 @@ fn serial_port() {
                                                          })
         .unwrap();
 
-    setup_serial_port(&mut serial_port.system_port()).unwrap();
-
     let mut events = Events::with_capacity(256);
-    let mut buf = [0; 256];
+    let buf = [0; 256];
 
     let poll = Poll::new().unwrap();
 
@@ -55,21 +45,59 @@ fn serial_port() {
         assert!(event.kind().is_writable());
         assert!(serial_port.write(&buf[..]).is_ok());
     }
+}
+
+#[test]
+fn poll_read() {
+    dotenv().ok();
+
+    let port_name = env::var("SERIAL_PORT")
+        .expect("Environment variable SERIAL_PORT must be specified");
+
+    let mut serial_port = SerialPort::open_with_settings(port_name.as_str(),
+                                                         &PortSettings {
+                                                             baud_rate: BaudRate::Baud115200,
+                                                             char_size: CharSize::Bits8,
+                                                             parity: Parity::ParityNone,
+                                                             stop_bits: StopBits::Stop1,
+                                                             flow_control:
+                                                                 FlowControl::FlowNone,
+                                                         })
+        .unwrap();
+
+    let mut events = Events::with_capacity(256);
+    let mut buf = [0; 256];
+
+    let poll = Poll::new().unwrap();
+    let mut timer = Timer::default();
+
+    timer.set_timeout(Duration::from_millis(5000), "timeout").unwrap();
 
     poll.register(&serial_port, Token(0), Ready::readable(), PollOpt::level()).unwrap();
+    poll.register(&timer, Token(1), Ready::readable(), PollOpt::edge()).unwrap();
 
     assert!(poll.poll(&mut events, None).is_ok());
     assert!(events.len() > 0);
 
     for event in &events {
-        assert_eq!(event.token(), Token(0));
-        assert!(event.kind().is_readable());
-        assert!(serial_port.read(&mut buf[..]).is_ok());
+        match event.token() {
+            Token(0) => {
+                assert!(event.kind().is_readable());
+                assert!(serial_port.read(&mut buf[..]).is_ok());
+            }
+            Token(1) => {
+                assert_eq!("timeout", timer.poll().unwrap());
+                assert_eq!(None, timer.poll());
+            }
+            _ => assert!(false)
+        }
     }
 
     let clone = serial_port.try_clone();
     assert!(clone.is_ok());
-    let mut clone_port = clone.unwrap();
+    let clone_port = clone.unwrap();
+
+    timer.set_timeout(Duration::from_millis(5000), "timeout").unwrap();
 
     poll.register(&clone_port, Token(0), Ready::readable(), PollOpt::level()).unwrap();
 
@@ -77,8 +105,16 @@ fn serial_port() {
     assert!(events.len() > 0);
 
     for event in &events {
-        assert_eq!(event.token(), Token(0));
-        assert!(event.kind().is_readable());
-        assert!(clone_port.read(&mut buf[..]).is_ok());
+        match event.token() {
+            Token(0) => {
+                assert!(event.kind().is_readable());
+                assert!(serial_port.read(&mut buf[..]).is_ok());
+            }
+            Token(1) => {
+                assert_eq!("timeout", timer.poll().unwrap());
+                assert_eq!(None, timer.poll());
+            }
+            _ => assert!(false)
+        }
     }
 }
